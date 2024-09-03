525be6529b7ed2ca59ea5d26ea5aa7c4aae37a33
Alex Manos <ajmanos1@gmail.com>
2024-09-03 15:06:56 -0400

    Update stats file

--- Ch1_Summarizing Univariate Data/Ch1.R
+++ Ch1_Summarizing Univariate Data/Ch1.R
@@ -4,12 +4,12 @@ x <- c(0.3, 0.9, 0.36, 0.92, 0.5, 1.0, 0.7, 9.7, 0.7, 1.3)
 # Compute the mean
 xBar <- mean(x)
 
-# Compute the median
+# Compute the median:
 Med <- median(x)
 
-# Compute the trimmed mean (50%, same as median)
+# Compute the trimmed mean (50%, same as median):
 trimMean <- mean(x, trim = 0.5)
 
-# Mode
+# Compute the mode:
 y <- table(as.vector(x))
 modeX <- as.numeric(names(y) [y == max(y)])
