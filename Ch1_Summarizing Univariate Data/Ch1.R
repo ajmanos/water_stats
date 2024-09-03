7375e6e49a6489bc4aed7ff2bc1a484567d4aba5
Alex Manos <ajmanos1@gmail.com>
2024-09-03 15:30:35 -0400

    Add stats examples

--- Ch1_Summarizing Univariate Data/Ch1.R
+++ Ch1_Summarizing Univariate Data/Ch1.R
@@ -1,11 +1,11 @@
 # Make some data:
-x <- c(0.3, 0.9, 0.36, 0.92, 0.5, 1.0, 0.7, 9.7, 0.7, 1.3)
+x <- c(2,4,8,9,11,11,12)
 
 # Compute the mean:
 xBar <- mean(x)
 
 # Compute the median:
-Med <- median(x)
+med <- median(x)
 
 # Compute the trimmed mean (50%, same as median):
 trimMean <- mean(x, trim = 0.5)
@@ -19,3 +19,47 @@ stdDev <- sd(x)
 
 # Compute the variance:
 varX <- var(x)
+
+# Calculate the interquartile range:
+quant <- as.numeric(quantile(x, type = 6))
+IQR <- quant[4] - quant[2]
+IQR <- quantile(x, probs = 0.75, type = 6) - quantile(x, probs = 0.25, type = 6)
+IQR <- IQR(x, type = 6)
+
+
+# Calculate the median absolute deviation:
+MAD <- median(abs(x-med))
+
+cat("IQR = ",IQR, ", MAD = ", MAD, "Standard Deviation = ", stdDev)
+
+# IQR =  7 , MAD =  2 Standard Deviation =  3.804759
+
+#------------------------------------------------------------------------------
+
+# Repeat steps with different dataset:
+x <- c(2,4,8,9,11,11,120)
+
+# Calculate the quantiles
+quant <- as.numeric(quantile(x, type = 6))
+
+# Calculate the interquartile range
+IQR <- quant[4] - quant[2]
+
+# Calculate the mean
+xBar <- mean(x)
+
+# Calculate the standard deviation
+stdDev <- sd(x)
+
+# Calculate the median
+med <- median(x)
+
+# calculate the median absolute deviation
+MAD <- median(abs(x-med))
+
+cat("IQR = ",IQR, ", MAD = ", MAD, "Standard Deviation = ", stdDev)
+
+# IQR =  7 , MAD =  2 Standard Deviation =  42.65699
+
+
+
